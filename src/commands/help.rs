6f7281e84795d79293e766d0f25b7312d302b78e
phoxwupsh <phoxwupsh@gmail.com>
2023-07-03 22:56:44 +0800

    Update help model for better customization

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -10,23 +10,25 @@ use crate::models::help::HELPS;
 
 #[command]
 async fn help(ctx: &Context, msg: &Message) -> CommandResult {
-    let waiting = msg.channel_id.send_message(ctx, |m| {
-        m.content("以下是所有能用的指令，你可以用`help 指令`來查看個指令的詳細用法。使用任何指令前面記得加上`!`。");
-        m.components(|c| {
-            c.create_action_row(|r| {
-                r.create_select_menu(|menu| {
-                    menu.custom_id("help");
-                    menu.placeholder("選一個指令");
-                    menu.options(|options| {
-                        for k in HELPS.keys() {
-                            options.create_option(|o| o.label(k).value(k));
-                        }
-                        options
+    let waiting = msg
+        .channel_id
+        .send_message(ctx, |message| {
+            message.content(&HELPS.help_msg).components(|components| {
+                components.create_action_row(|row| {
+                    row.create_select_menu(|menu| {
+                        menu.custom_id("help")
+                            .placeholder(&HELPS.placeholder)
+                            .options(|options| {
+                                for k in HELPS.command_helps.keys() {
+                                    options.create_option(|o| o.label(k).value(k));
+                                }
+                                options
+                            })
                     })
                 })
             })
         })
-    }).await?;
+        .await?;
 
     let interaction = match waiting
         .await_component_interaction(ctx)
@@ -41,20 +43,24 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
     };
 
     let command_name = &interaction.data.values[0];
-    let target_help = HELPS.get(command_name).expect("Error loading command help");
+    let target_help = HELPS
+        .command_helps
+        .get(command_name)
+        .expect("Error loading command help");
 
     interaction
         .create_interaction_response(ctx, |resp| {
             resp.kind(InteractionResponseType::UpdateMessage)
                 .interaction_response_data(|data| {
-                    data.content(format!("以下是`{}`的使用方式：", command_name));
-                    data.components(|c| c);
-                    data.embed(|e| {
-                        e.title(target_help.command_name.clone());
-                        e.description(target_help.description.clone());
-                        e.field("用法", target_help.usage.clone(), true);
-                        e.field("範例", target_help.example.join("\n"), true)
-                    })
+                    data.content(&target_help.help_msg)
+                        .components(|components| components)
+                        .embed(|embed| {
+                            embed
+                                .title(target_help.command_name.clone())
+                                .description(target_help.description.clone())
+                                .field(&HELPS.usage_field, target_help.usage.clone(), true)
+                                .field(&HELPS.example_field, target_help.example.join("\n"), true)
+                        })
                 })
         })
         .await?;
--- src/models/help.rs
+++ src/models/help.rs
@@ -5,6 +5,16 @@ use lazy_static::lazy_static;
 
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub struct Help {
+    pub help_msg: String,
+    pub placeholder: String,
+    pub usage_field: String,
+    pub example_field: String,
+    pub command_helps: HashMap<String, CommandHelp>
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct CommandHelp {
+    pub help_msg: String,
     pub command_name: String,
     pub description: String,
     pub usage: String,
@@ -12,9 +22,9 @@ pub struct Help {
 }
 
 lazy_static!{
-    pub static ref HELPS: HashMap<String, Help> = {
+    pub static ref HELPS: Help = {
         let helps_json = fs::read_to_string("helps.json").expect("Error loading helps.json");
-        let helps: HashMap<String, Help> = serde_json::from_str(&helps_json).expect("Error parsing helps.json");
+        let helps: Help = serde_json::from_str(&helps_json).expect("Error parsing helps.json");
         helps
     };
 }
