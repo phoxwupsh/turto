8ac7a64c591cd89d25d6258b2df20b6cc7db3af3
phoxwupsh <phoxwupsh@gmail.com>
2023-07-13 12:31:25 +0800

    Help message disappear instead of notify timed out

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -43,7 +43,7 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
     {
         Some(x) => x,
         None => {
-            msg.reply(ctx, "Timed out").await?;
+            msg.delete(ctx).await?;
             return Ok(());
         }
     };
