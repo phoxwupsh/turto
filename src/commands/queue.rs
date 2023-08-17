7c6a385c95a07d91ddfcdab3e3255964dc1ca55a
phoxwupsh <phoxwupsh@gmail.com>
2023-08-18 00:52:49 +0800

    Adding  a feature allowing user add multiple items into the playlist

--- src/commands/queue.rs
+++ src/commands/queue.rs
@@ -6,32 +6,37 @@ use serenity::{
 
 use crate::{
     guild::playlist::Playlists,
-    models::{metadata::Metadata, playlist::Playlist},
+    models::{playlist_item::PlaylistItem, playlist::Playlist},
 };
 
 #[command]
 #[bucket = "music"]
-async fn queue(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    let url = args.rest();
-    let source = songbird::input::ytdl(&url).await?;
-    let metadata = source.metadata.clone();
+async fn queue(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    while !args.is_empty() {
+        let url = args.single::<String>().unwrap();
+        let Ok(source) = songbird::input::ytdl(&url).await else {
+            msg.reply(ctx, format!("Cannot find `{}`", url)).await?;
+            continue;
+        };
+        let metadata = source.metadata.clone();
 
-    let playlists_lock = ctx
-        .data
-        .read()
-        .await
-        .get::<Playlists>()
-        .expect("Expected Playlists in TypeMap.")
-        .clone();
-    {
-        let mut playlists = playlists_lock.lock().await;
-        let playlist = playlists
-            .entry(msg.guild_id.expect("Expected guild_id"))
-            .or_insert_with(Playlist::new);
+        let playlists_lock = ctx
+            .data
+            .read()
+            .await
+            .get::<Playlists>()
+            .expect("Expected Playlists in TypeMap.")
+            .clone();
+        {
+            let mut playlists = playlists_lock.lock().await;
+            let playlist = playlists
+                .entry(msg.guild_id.expect("Expected guild_id"))
+                .or_insert_with(Playlist::new);
 
-        msg.reply(ctx, format!("✅ {}", metadata.title.clone().unwrap()))
-            .await?;
-        playlist.push_back(Metadata::from(*metadata)); // Add song to playlist
+            msg.reply(ctx, format!("✅ {}", metadata.title.clone().unwrap()))
+                .await?;
+            playlist.push_back(PlaylistItem::from(*metadata)); // Add song to playlist
+        }   
     }
     Ok(())
 }
