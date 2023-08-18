a6eaf754cf404e90120273b6102aa2328b49c133
phoxwupsh <phoxwupsh@gmail.com>
2023-08-18 17:41:29 +0800

    Add youtube playlist support

--- src/models/playlist.rs
+++ src/models/playlist.rs
@@ -1,5 +1,72 @@
-use std::collections::VecDeque;
+use std::{
+    collections::{VecDeque, vec_deque::IntoIter},
+    ops::{Deref, DerefMut}, process::{Command, Stdio}, io::{BufRead, BufReader},
+};
 
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
 use super::playlist_item::PlaylistItem;
 
-pub type Playlist = VecDeque<PlaylistItem>;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Playlist(VecDeque<PlaylistItem>);
+
+impl Playlist {
+    pub fn new() -> Self {
+        Playlist(VecDeque::<PlaylistItem>::new())
+    }
+
+    pub fn ytdl_playlist(url: &str) -> Option<Playlist> {
+        let args = vec![url, "--flat-playlist", "-j"];
+    
+        let mut child = Command::new("yt-dlp")
+            .args(args)
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+    
+        let Some(stdout) = &mut child.stdout else {
+            return None;
+        };
+    
+        let reader = BufReader::new(stdout);
+    
+        let mut res = Playlist::new();
+    
+        for line in reader.lines().flatten() {
+            if let Ok(value) = serde_json::from_str::<Value>(&line) {
+                if let Some(new_playlist_item) = PlaylistItem::try_from_ytdl_output(&value) {
+                    res.push_back(new_playlist_item);
+                }
+            }
+        }
+        Some(res)
+    }
+}
+
+impl Deref for Playlist {
+    type Target = VecDeque<PlaylistItem>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Playlist {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Default for Playlist {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl IntoIterator for Playlist {
+    type Item = PlaylistItem;
+    type IntoIter = IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
--- src/models/playlist_item.rs
+++ src/models/playlist_item.rs
@@ -1,26 +1,69 @@
 use std::time::Duration;
 
-use serde::{Serialize,Deserialize
-};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use songbird::input::Metadata;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct PlaylistItem{
+pub struct PlaylistItem {
     pub url: String,
     pub title: String,
     pub channel: String,
     pub duration: Duration,
-    pub thumbnail: String
+    pub thumbnail: String,
 }
 
 impl From<Metadata> for PlaylistItem {
     fn from(value: Metadata) -> Self {
-        PlaylistItem { 
-            url: value.source_url.unwrap_or_default(), 
-            title: value.title.unwrap_or_default(), 
-            channel: value.channel.unwrap_or_default(), 
-            duration: value.duration.unwrap_or_default(), 
-            thumbnail: value.thumbnail.unwrap_or_default() 
+        PlaylistItem {
+            url: value.source_url.unwrap_or_default(),
+            title: value.title.unwrap_or_default(),
+            channel: value.channel.unwrap_or_default(),
+            duration: value.duration.unwrap_or_default(),
+            thumbnail: value.thumbnail.unwrap_or_default(),
         }
+    }
+}
+
+impl PlaylistItem {
+    pub fn try_from_ytdl_output(value: &Value) -> Option<Self> {
+        let obj = value.as_object()?;
+
+        let url = obj
+            .get("webpage_url")
+            .and_then(Value::as_str)
+            .map(str::to_string)?;
+
+        let title = obj
+            .get("title")
+            .and_then(Value::as_str)
+            .map(str::to_string)?;
+
+        let channel = obj
+            .get("channel")
+            .and_then(Value::as_str)
+            .map(str::to_string)?;
+
+        let duration = obj
+            .get("duration")
+            .and_then(Value::as_f64)
+            .map(Duration::from_secs_f64)?;
+
+        let thumbnail = obj
+            .get("thumbnails")
+            .and_then(Value::as_array)
+            .map(|t| t.last().unwrap_or(&Value::Null))
+            .and_then(Value::as_object)
+            .and_then(|t|t.get("url"))
+            .and_then(Value::as_str)
+            .map(str::to_string)?;
+
+        Some(Self {
+            url,
+            title,
+            channel,
+            duration,
+            thumbnail,
+        })
     }
 }
