400eff162bce7dc4b96417f75c8a9924c2d735be
phoxwupsh <phoxwupsh@gmail.com>
2024-03-08 00:10:12 +0800

    Code cleaning

--- src/utils/ytdl.rs
+++ src/utils/ytdl.rs
@@ -1,56 +1,66 @@
 use crate::models::{playlist_item::PlaylistItem, youtube_playlist::YouTubePlaylist};
-use serde_json::Value;
+use serde_json::{Map, Value};
 use std::{
     io::{BufRead, BufReader},
     process::{Command, Stdio},
 };
+use tracing::error;
 
 pub fn ytdl_playlist(url: &str) -> Option<YouTubePlaylist> {
     let args = vec![url, "--flat-playlist", "-j"];
 
-    let mut child = Command::new("yt-dlp")
+    let child = match Command::new("yt-dlp")
         .args(args)
         .stdout(Stdio::piped())
         .spawn()
-        .unwrap_or_else(|err| panic!("yt-dlp command failed to run: {}", err));
-
-    let Some(stdout) = &mut child.stdout else {
-        return None;
+    {
+        Ok(child) => child,
+        Err(err) => {
+            error!("Failed to run yt-dlp: {}", err);
+            return None;
+        }
     };
 
-    let reader = BufReader::new(stdout);
+    let Some(reader) = child.stdout.map(BufReader::new) else {
+        error!("Failed to read stdout");
+        return None;
+    };
 
     let mut res = YouTubePlaylist::new();
     let mut iter_read = reader.lines().flatten();
 
-    if let Some(first) = iter_read.next() {
-        if let Ok(value) = serde_json::from_str::<Value>(&first) {
-            let obj = value.as_object()?;
-            res.playlist_id = obj
-                .get("playlist_id")
-                .and_then(Value::as_str)
-                .map(str::to_string);
-            res.title = obj
-                .get("playlist_title")
-                .and_then(Value::as_str)
-                .map(str::to_string);
-            res.author = obj
-                .get("playlist_uploader")
-                .and_then(Value::as_str)
-                .map(str::to_string);
-            if let Some(first_) = PlaylistItem::try_from_ytdl_output(&value) {
-                res.push_back(first_);
-            }
-        }
-    } else {
+    let Some(first) = iter_read.next() else {
         return None;
+    };
+    let obj = match serde_json::from_str::<Map<String, Value>>(&first) {
+        Ok(obj) => obj,
+        Err(err) => {
+            error!("Failed to parse yt-dlp output: {}", err);
+            return None;
+        }
+    };
+
+    res.playlist_id = obj
+        .get("playlist_id")
+        .and_then(Value::as_str)
+        .map(str::to_string);
+    res.title = obj
+        .get("playlist_title")
+        .and_then(Value::as_str)
+        .map(str::to_string);
+    res.author = obj
+        .get("playlist_uploader")
+        .and_then(Value::as_str)
+        .map(str::to_string);
+    if let Some(first_) = PlaylistItem::try_from_ytdl_output(&obj) {
+        res.push_back(first_);
     }
 
     for line in iter_read {
-        if let Ok(value) = serde_json::from_str::<Value>(&line) {
-            if let Some(new_playlist_item) = PlaylistItem::try_from_ytdl_output(&value) {
-                res.push_back(new_playlist_item);
-            }
+        if let Ok(Some(new_playlist_item)) = serde_json::from_str::<Map<String, Value>>(&line)
+            .map(|value| PlaylistItem::try_from_ytdl_output(&value))
+        {
+            res.push_back(new_playlist_item);
         }
     }
     Some(res)
