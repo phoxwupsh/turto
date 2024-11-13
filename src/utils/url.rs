46083933260b5836d1ac8d6264d25eb94fae5a12
ButterCatz <zombies0309@gmail.com>
2024-11-13 17:41:46 +0800

    add support for youtube music playlist

--- src/utils/url.rs
+++ src/utils/url.rs
@@ -7,7 +7,7 @@ pub trait UrlExt {
 impl UrlExt for Url {
     fn is_yt_playlist(&self) -> bool {
         match self.host_str() {
-            Some("www.youtube.com") | Some("youtube.com") | Some("youtu.be") => {
+            Some("www.youtube.com") | Some("youtube.com") | Some("youtu.be") | Some("music.youtube.com") => {
                 self.query_pairs().any(|(k, _)| k == "list")
             }
             _ => false,
